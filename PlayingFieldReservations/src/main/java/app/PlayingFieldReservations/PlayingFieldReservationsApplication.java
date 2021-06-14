35fc07af3d9d33139bec5cc1e9fc4859d7ee7289
Izabel96 <izabel1796@yahoo.bg>
2021-06-14 18:42:04 +0300

    Add files via upload

---
+++ PlayingFieldReservations/src/main/java/app/PlayingFieldReservations/PlayingFieldReservationsApplication.java
@@ -0,0 +1,13 @@
+package app.PlayingFieldReservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingFieldReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingFieldReservationsApplication.class, args);
+	}
+
+}
